118033740b2abc751557cbd934b8c90e992b8d4d
himanshu <bhatth941@gmail.com>
2023-03-19 12:11:34 +0545

    Added customized package

---
+++ Package/customize.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Customize</title>
+    <style>
+    
+
+    </style>
+</head> 
+<h1 style="text-align: center;"  >Customize</h1>
+    <div class="container">
+
+    <img  src="icons/user.png" alt="notification" width="25px" height="25px"  align="right"  >
+    <img  src="icons/notification.png" alt="notification" width="25px" height="25px"  align="right"  >
+    <img  src="icons/logo.png" alt="notification" width="50px" height="50px"  align="left" id="logo" >
+    <br>
+    <br>
+    <p>
+        Name: <br>
+         <input type="text" name="FName" placeholder="First Name"> 
+            <input  type="text" name="LName"  id="LName" placeholder="Last Name">
+    </p>
+   
+    <p> Trip to:
+        <br>
+         <input type="text" id="trip_To"  ></p>
+    
+    <p>Guest <br>
+           <input type="text" id="guest" ></p>
+    
+    <p>Date <br>
+         <input type="date" id="Date" ></p>
+    
+    <p>Transportation <br>
+        <input type="text" id="Transportation" id=""></p>
+    
+    <p>Contact <br>
+        <input type="number" id="Contact" ></p>
+    
+    <input type="submit" value="Customize" id="submit">
+    
+</div>
+</body>
+</html>
