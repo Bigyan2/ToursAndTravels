04d6c09e4f7f29fbfcedd8f27675a1014aaee31b
himanshu <bhatth941@gmail.com>
2023-03-24 07:06:58 +0545

    changes made

--- Package/customize.php
+++ Package/customize.php
@@ -4,22 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
     <link rel="stylesheet" href="style.css">
     <title>Customize</title>
     <style>
     
+
     </style>
 </head> 
-<h1 style="text-align: center;"  >Customize</h1>
+<body>
+   
+<img  src="icons/logo.png" alt="logo" width="50px" height="50px" id="logo" >
+<h1>Customize</h1>
     <div class="container">
+        <!-- <img  src="icons/logo.png" alt="logo" width="50px" height="50px" id="logo" > -->
 
-    <img  src="icons/user.png" alt="notification" width="25px" height="25px"  align="right"  >
-    <img  src="icons/notification.png" alt="notification" width="25px" height="25px"  align="right"  >
-    <img  src="icons/logo.png" alt="notification" width="50px" height="50px"  align="left" id="logo" >
-    <br>
+    <img  src="icons/user.png" alt="notification" width="25px" height="25px"  id="profile"  >
+    <!-- <img  src="icons/notification.png" alt="notification" width="25px" height="25px"  id="notification"  > -->
+    
     <br>
     <p>
-        Name: <br>
+        name : <br>
          <input type="text" name="FName" placeholder="First Name"> 
             <input  type="text" name="LName"  id="LName" placeholder="Last Name">
     </p>
@@ -28,16 +33,17 @@
         <br>
          <input type="text" id="trip_To"  ></p>
     
-    <p>Guest <br>
+    <p >Guest : <br>
            <input type="text" id="guest" ></p>
+    <div id="Date" >
+    <p ></p>Date : <br>
+         <input type="date"  ></p>
+        </div>
     
-    <p>Date <br>
-         <input type="date" id="Date" ></p>
+    <p>Transportation : <br>
+        <input type="text" id="Transportation"></p>
     
-    <p>Transportation <br>
-        <input type="text" id="Transportation" id=""></p>
-    
-    <p>Contact <br>
+    <p>Contact : <br>
         <input type="number" id="Contact" ></p>
     
     <input type="submit" value="Customize" id="submit">
