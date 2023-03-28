8cb26aeadf53c44d24610f19224640ec4ed4322d
Anil Yogi <np03cs4s220204@heraldcollege.edu.np>
2023-03-28 15:21:14 +0545

    changed login signup and packages

---
+++ logincreate/suggestionLocation.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="suggestionLocation.css">
+    <script
+      src="https://kit.fontawesome.com/97f454a94a.js"
+      crossorigin="anonymous"
+    ></script>
+    <title>Suggestion Location</title>
+  </head>
+  <body>
+    <div class="conatainer">
+      <div class="top_navbar">
+        <div class="logo">
+          <img src="logo.png" alt="logo" />
+        </div>
+        <div class="nav_button">
+          <a  href="#Home">Home</a>
+          <a class="active" href="#Package">Package</a>
+          <a href="#Booking">Booking</a>
+          <a href="#Hotel">Hotel</a>
+        </div>
+        <div class="search">
+          <form action="/action_page.php" class="search_box">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </form>
+        </div>
+        <div class="profile">
+          <i class="fa-solid fa-user"></i>
+        </div>
+      </div>
+
+      <h1>Suggested Locations</h1>
+      <div class="location_box">
+        <div class="locations">
+          <img src="https://source.unsplash.com/1200x600/?pokhara" alt="nature">
+          <h3>Pokhara</h3>
+          <p>You will never be able to get enough of Pokhara.” “She told me Pokhara is so beautiful that you will experience a myriad of unforgettable moments. You will never be able to get enough of Pokhara.” “Pokhara has a magical serenity that is not found in any other place!”;
+  
+          </p>
+        </div>
+        <div class="locations">
+          <img src="https://source.unsplash.com/1200x600/?annapurna" alt="annapurna">
+          <h3>Annapurna</h3>
+          <p>You will never be able to get enough of Pokhara.” “She told me Pokhara is so beautiful that you will experience a myriad of unforgettable moments. You will never be able to get enough of Pokhara.” “Pokhara has a magical serenity that is not found in any other place!”;
+            
+          </p>
+        </div>
+        <div class="locations">
+          <img src="https://source.unsplash.com/1200x600/?dolpa" alt="nature">
+          <h3>Dolpa</h3>
+          <p>You will never be able to get enough of Pokhara.” “She told me Pokhara is so beautiful that you will experience a myriad of unforgettable moments. You will never be able to get enough of Pokhara.” “Pokhara has a magical serenity that is not found in any other place!”;
+  
+          </p>
+        </div>
+        <div class="locations">
+          <img src="https://source.unsplash.com/1200x600/?manang" alt="nature">
+          <h3>Manang</h3>
+          <p>You will never be able to get enough of Pokhara.” “She told me Pokhara is so beautiful that you will experience a myriad of unforgettable moments. You will never be able to get enough of Pokhara.” “Pokhara has a magical serenity that is not found in any other place!”;
+            
+          </p>
+        </div>
+        <div class="locations">
+          <img src="https://source.unsplash.com/1200x600/?Kathmandu" alt="nature">
+          <h3>Kathmandu</h3>
+          <p>You will never be able to get enough of Pokhara.” “She told me Pokhara is so beautiful that you will experience a myriad of unforgettable moments. You will never be able to get enough of Pokhara.” “Pokhara has a magical serenity that is not found in any other place!”;
+  
+          </p>
+        </div>
+        <div class="locations">
+          <img src="https://source.unsplash.com/1200x600/?Jhapa" alt="nature">
+          <h3>Jhapa</h3>
+          <p>You will never be able to get enough of Pokhara.” “She told me Pokhara is so beautiful that you will experience a myriad of unforgettable moments. You will never be able to get enough of Pokhara.” “Pokhara has a magical serenity that is not found in any other place!”;
+            
+          </p>
+        </div>
+      </div>
+
+     <div class="customize_container">
+      <p>You can also customize your booking at the place you want to go.</p>
+      <button class="customize_button">Customize</button>
+       
+     </div>
+
+      
+    </div>
+  </body>
+</html>
